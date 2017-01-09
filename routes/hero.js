b996db7182ea9ab6699a186065fdc2891f0eaab8
Karla Hugo <karlabrandon817@gmail.com>
2017-01-09 14:41:02 -0600

    first commit

---
+++ routes/hero.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// get all heros
+router.get('/', function(req, res) {
+
+});
+
+// post to create a new hero
+router.post('/', function(req, res) {
+
+});
+
+module.exports = router;
