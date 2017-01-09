479b232c046cf7e134f23c3eaeef8eb4e17b852c
Karla Hugo <karlabrandon817@gmail.com>
2017-01-09 15:17:09 -0600

    displaying on DOM

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,42 @@
 console.log('sourced');
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('HeroController', ['$scope', '$http', function($scope, $http) {
+    $scope.getHero = function() {
+        $http({
+            method: 'GET',
+            url: '/hero'
+        }).then(function(response) {
+            console.log('response:', response);
+            $scope.heroResponse = response.data;
+        }); //end get call
+    }; //end $scope.getHero
+
+    $scope.addHero = function() {
+        console.log('in addHero');
+        var addHero = {
+            alias: $scope.aliasIn,
+            first_name: $scope.first_nameIn,
+            last_name: $scope.last_nameIn,
+            city: $scope.cityIn,
+            power_name: $scope.powerIn
+        }; //end addHero
+        console.log('adding:', addHero);
+        //-- clear inputs on button click -- //
+        $scope.aliasIn = '';
+        $scope.first_nameIn = '';
+        $scope.last_nameIn = '';
+        $scope.cityIn = '';
+        $scope.powerIn = '';
+        //end input clear
+        $http({
+            method: 'POST',
+            url: '/hero',
+            data: addHero
+        }).then(function(response) {
+            $scope.getHero();
+        }); //end $scope.addHero
+    }; //end $scope.addHero
+    $scope.getHero();
+}]); //end HeroController
--- routes/hero.js
+++ routes/hero.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Heroes = require('../models/heroModel.js');
 
 // get all heros
 router.get('/', function(req, res) {
-
-});
+    Heroes.find({}, function(err, heroesResults) {
+        if (err) {
+            console.log('error in router.get:', err);
+            res.sendStatus(500);
+        } else {
+            console.log('heroesResults: ', heroesResults);
+            res.send(heroesResults);
+        }
+    }); //end Heroes.find
+}); //end router.get
 
 // post to create a new hero
 router.post('/', function(req, res) {
+    console.log('req.body in router.post: ', req.body);
+    var sentData = req.body;
 
-});
+    var newHero = new Heroes({
+        alias: sentData.alias,
+        first_name: sentData.first_name,
+        last_name: sentData.last_name,
+        city: sentData.city,
+        power_name: sentData.power_name
+    }); //end newPet
+
+    newHero.save(function(err) {
+        if (err) {
+            console.log('error saving hero: ', err);
+            res.sendStatus(500);
+        } else {
+            console.log('a new hero was added!');
+            res.sendStatus(201);
+        }
+    }); //end newHero.save
+}); //end router.post
 
 module.exports = router;
